70f742917c9ca95d47a58381366228306caded75
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-08-20 14:51:43 -0300

    adiciona entidades iniciais (Produto, Carrinho, ItemCarrinho) no domínio

---
+++ src/dominios/entidades/carrinho.entidade.ts
@@ -0,0 +1,6 @@
+class CarrinhoEntidade {
+  id: string;
+  usuarioId: string;
+}
+
+export default CarrinhoEntidade;
---
+++ src/dominios/entidades/item-carrinho.entidade.ts
@@ -0,0 +1,8 @@
+class ItemCarrinhoEntidade {
+  id: string;
+  produtoId: string;
+  carrinhoId: string;
+  quantidade: number;
+}
+
+export default ItemCarrinhoEntidade;
