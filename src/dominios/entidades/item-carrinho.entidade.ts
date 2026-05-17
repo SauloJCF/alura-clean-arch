e6ed24c51cfce108ccd1382e77ece0973a4756b0
Saulo.Figueiredo <saulojcf@gmail.com>
2026-05-17 09:18:57 -0300

    Cria entidades domínio

---
+++ src/dominios/entidades/item-carrinho.entidade.ts
@@ -0,0 +1,8 @@
+class ItemCarrinhoEntidade {
+    id: string;
+    quantidade: number;
+    produtoId: string;
+    carrinhoId: string;
+}
+
+export default ItemCarrinhoEntidade;
---
+++ src/dominios/entidades/produto.entidade.ts
@@ -0,0 +1,8 @@
+class ProdutoEntidade {
+    id: string;
+    nome: string;
+    preco: number;
+    estoque: number;
+}
+
+export default ProdutoEntidade;
