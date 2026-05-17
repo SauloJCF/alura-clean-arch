754e498defef549cd660ca53bd0be1f618f34529
Saulo.Figueiredo <saulojcf@gmail.com>
2026-05-17 12:54:59 -0300

    Importar módulos faltantes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProdutoModule } from './infraestrutura/modulos/produto.module';
+import { CarrinhoModule } from './infraestrutura/modulos/carrinho.module';
 @Module({
-  imports: [],
+  imports: [ProdutoModule, CarrinhoModule],
   controllers: [AppController],
   providers: [AppService],
 })
