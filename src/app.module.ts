e50cb29748d5ab688fe1710d5c95abbce091d2ce
Saulo.Figueiredo <saulojcf@gmail.com>
2026-05-23 11:34:48 -0300

    Remove arquivos desnecessários. Ajuste tsconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProdutoModule } from './infraestrutura/modulos/produto.module';
 import { CarrinhoModule } from './infraestrutura/modulos/carrinho.module';
 @Module({
   imports: [ProdutoModule, CarrinhoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
